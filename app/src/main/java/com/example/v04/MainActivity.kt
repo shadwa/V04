cdfa20af9cb8de512637209c9a54da1fe8aceaf6
shadwa <wwa_50@mail.ru>
2021-11-16 23:01:09 +0700

    V04

--- app/src/main/java/com/example/v04/MainActivity.kt
+++ app/src/main/java/com/example/v04/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.v04
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+private lateinit var textViewResult: TextView
+private const val TAG = "MyApp"
 class MainActivity : AppCompatActivity() {
+    var textViewResult: TextView? = null
+    var button: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textViewResult=findViewById(R.id.textViewResult)
+        button=findViewById(R.id.button)
+
     }
+    fun task (view: View) {
+        view as Button
+
+        with(textViewResult) {
+            this?.setText(myString())
+            this?.setGravity(Gravity.CENTER);
+        }
+    }
+    fun myString(): String{
+        val chars = "abcdefghijklmnopqrstuvwxyz".toCharArray()
+        var stroka:String=""
+        var n:Int=chars.size
+        for (i in 0..n-1){
+            if (i % 2 >0) {
+                stroka=stroka+chars.get(i)+' '
+            }
+        }
+        return stroka
+
+
+    }
+
 }
